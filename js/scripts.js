e5c7452b6fa71c6ae8a22db6deba5093d8dc6d8c
Sean Byram <seanbyram@icloud.com>
2019-01-25 10:22:36 -0800

    update UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
 // Business Logic
-
+var number = "";
 function beepboop(number) {
 
-  result = "";
-  for(i = 1; i <= number; i++)
-    if (number.includes === "3") {
+  // result = "";
+  // for(i = 1; i <= number; i++)
+    if (number.includes(3)) {
       console.log("sorry dave");
+    } else if (number.includes(2)) {
+      console.log("boop");
+    } else if (number.includes(1)) {
+      console.log("beep");
     } else {
-
+      console.log(number);
     }
 }
 
@@ -19,33 +23,36 @@ function beepboop(number) {
 $(document).ready(function() {
 
   $("form#formInput").submit(function(event) {
-  alert("test");
+
     event.preventDefault();
 
-    $("#split-results").empty();
+    var number = $("#number-input").val();
 
-    var inputNumber = $("input").val();
+    beepboop(number);
 
-    if (inputNumber <= 0) {
-     $(".error-message").show();
-     $("#error-message").text("Please use only positive numbers");
-   } else if (inputNumber > 100){
-    beepboop(inputNumber);
-    $(".display-title").show();
-    $("#split-results").append("<div class='col-md-2'></div>");
-  }
+
+
   });
 
 
 
 
 
-
-
-
-
-
-
-
-
 });
+
+
+
+
+//
+// $("#split-results").empty();
+//
+// var inputNumber = $("input").val();
+//
+// if (inputNumber <= 0) {
+//  $(".error-message").show();
+//  $("#error-message").text("Please use only positive numbers");
+// } else if (inputNumber > 100){
+// beepboop(inputNumber);
+// $(".display-title").show();
+// $("#split-results").append("<div class='col-md-2'></div>");
+// }
