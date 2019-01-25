0b058ca0355a0d55891c1cfae882b39d1ba9359f
Sean Byram <seanbyram@icloud.com>
2019-01-25 13:22:22 -0800

    add logic for displaying results. add logic for error message

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 var number = "";
 function beepboop(number) {
 
-  // result = "";
-  // for(i = 1; i <= number; i++)
+  result = "";
+  for(i = 1; i <= number; i++)
     if (number.includes(3)) {
-      console.log("sorry dave");
+      $("#result").append(" sorry dave");
     } else if (number.includes(2)) {
       console.log("boop");
     } else if (number.includes(1)) {
@@ -34,6 +34,36 @@ $(document).ready(function() {
 
   });
 
+  if (inputNumber <= 0) {
+       $(".error-message").show();
+       $("#error-message").text("Please use only positive numbers");
+     } else if (inputNumber > 100){
+        beepboop(inputNumber);
+        $(".display-title").show();
+        $("#split-results").append("<div class='col-md-2'></div>");
+
+        var columnLimit = Math.ceil(inputNumber/8);
+
+        for (var i = 0; i < inputNumber; i+=columnLimit) {
+          var section = result.slice(i, i+columnLimit);
+
+          displayResults(section);
+
+          $("#split-results").append("<div class='col-md-1'><span id='display-results"+[i]+"'  class='display-results'></span></div>");
+
+          $("#display-results" + [i]).append(listResults);
+        };
+        $("#split-results").append("<div class='col-md-2'></div>").fadeIn();
+      } else {
+          beepboop(inputNumber);
+          $(".display-title").show();
+          $("#split-results").append("<div class='col-md-2'></div>");
+          displayResults(result);
+          $("#split-results").append("<div class='col-md-1'><span id='display-results'  class='display-results'></span></div>");
+          $("#display-results").append(listResults).slideDown();
+      }
+    });
+  });
 
 
 
