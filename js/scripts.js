f4414096570078d9aa86835d843d7be0ddfa09c2
Sean Byram <seanbyram@icloud.com>
2019-01-25 10:09:45 -0800

    update backendlogic to process user-generated number

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
-
 // Business Logic
 
+function beepboop(number) {
+
+  result = "";
+  for(i = 1; i <= number; i++)
+    if (number.includes === "3") {
+      console.log("sorry dave");
+    } else {
+
+    }
+}
 
 
 
@@ -9,7 +18,34 @@
 
 $(document).ready(function() {
 
-  
+  $("form#formInput").submit(function(event) {
+  alert("test");
+    event.preventDefault();
+
+    $("#split-results").empty();
+
+    var inputNumber = $("input").val();
+
+    if (inputNumber <= 0) {
+     $(".error-message").show();
+     $("#error-message").text("Please use only positive numbers");
+   } else if (inputNumber > 100){
+    beepboop(inputNumber);
+    $(".display-title").show();
+    $("#split-results").append("<div class='col-md-2'></div>");
+  }
+  });
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
